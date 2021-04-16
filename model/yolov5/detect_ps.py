c7cdc33f9151368ea76d3386aba7006901079fc8
Luistoq <63573157+Luistoq@users.noreply.github.com>
2021-04-16 11:57:51 +0100

    Update detect_ps.py

--- model/yolov5/detect_ps.py
+++ model/yolov5/detect_ps.py
@@ -167,7 +167,7 @@ def detect(save_img=False):
 
     if save_txt or save_img:
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        print(f"Results saved to runs/detect/ins_points")
+        print(f"Results saved to runs/detect/pipe_specs")
 
     print(f'Done. ({time.time() - t0:.3f}s)')
 
